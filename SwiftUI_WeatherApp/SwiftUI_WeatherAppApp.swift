cac6e29e4211699eb437deaaba09cafed0c14e6d
roargenie <lazyhoper987@gmail.com>
2023-04-07 16:31:23 +0900

    git init

---
+++ SwiftUI_WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI_WeatherApp
+//
+//  Created by 이명진 on 2023/04/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
