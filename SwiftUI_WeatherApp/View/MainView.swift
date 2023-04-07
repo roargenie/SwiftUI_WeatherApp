f1750d318e7b41bb7615ebfbcf5172ded98c83ec
roargenie <lazyhoper987@gmail.com>
2023-04-07 16:45:55 +0900

    프로젝트 초기 셋팅

---
+++ SwiftUI_WeatherApp/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  SwiftUI_WeatherApp
+//
+//  Created by 이명진 on 2023/04/07.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
