6a76f957b04f78d77daf28e145dd66fc76c32544
roargenie <lazyhoper987@gmail.com>
2023-04-10 17:51:16 +0900

    [Add] 메인 뷰 레이아웃 구성

--- SwiftUI_WeatherApp/View/MainView.swift
+++ SwiftUI_WeatherApp/View/MainView.swift
@@ -10,7 +10,44 @@ import SwiftUI
 struct MainView: View {
     
     var body: some View {
-        Text("")
+        ZStack {
+            GeometryReader { reader in
+                Image("bkg")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: reader.size.width)
+                    .ignoresSafeArea()
+                    .overlay {
+                        Color.black.opacity(0.5)
+                            .ignoresSafeArea()
+                    }
+            }
+            
+            VStack {
+                Text("강남역")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 60)
+                
+                GeometryReader { reader in
+                    ScrollView(showsIndicators: false) {
+                        VStack {
+                            Color.red
+                            
+                            Color.yellow
+                                .frame(height: 200)
+                        }
+                        .frame(height: reader.size.height)
+                        
+                        Spacer(minLength: reader.safeAreaInsets.bottom)
+                        
+                        Color.green
+                            .frame(height: 1000)
+                    }
+                }
+            }
+        }
         
     }
 }
